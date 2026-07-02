2e61e45e21ac228e6cbefec0128150800d867c16
Hadeel Ismail <ismailhadeel06@gmail.com>
2026-07-02 19:15:26 +0300

    Add files via upload

---
+++ Break Time Program.py
@@ -0,0 +1,11 @@
+import webbrowser
+import time
+start_time=time.ctime()
+start=1
+target=10
+while start<=target:
+    webbrowser.open("https://youtu.be/NqDO5zOqnFM?si=NM3FGs-byPZNAN_u")
+    time.sleep(60)
+    start=start+1
+
+
